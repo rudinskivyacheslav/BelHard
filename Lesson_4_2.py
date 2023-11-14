2019d0aa13ef0d78954094e386db212c1a582690
rudinskivyacheslav <sslavabox@gmail.com>
2023-11-14 09:17:02 +0300

    new lesson_4_2

--- Lesson_4_2.py
+++ Lesson_4_2.py
@@ -1,9 +1,9 @@
 #Без использования collections, написать программу, которая будет создавать словарь для
 # подсчитывания количества вхождений каждой буквы в текст введенный с клавиатуры
 
-text = input("Enter text: ").replace(" ", "")
+text = input("Enter text: ")
 
-dic = {i: text.count((i)) for i in set(text)}
+dic = {i: text.count((i)) for i in set(text) if i.isalpha()}
 
 for i in dic:
     print(f"{i}: {dic[i]}")
