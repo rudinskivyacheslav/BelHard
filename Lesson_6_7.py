cd43051a9a18601bad802ddcb1e578f7157b5999
rudinskivyacheslav <sslavabox@gmail.com>
2023-11-16 08:40:28 +0300

    new lesson_6_7

---
+++ Lesson_6_7.py
@@ -0,0 +1,14 @@
+# Дан список чисел, необходимо его развернуть без использования метода revese и функции reversed,
+# а так же дополнительного списка и среза
+from math import floor
+
+
+def reverce_list(lst: list) -> list:
+    for i in range(floor(len(lst)/2)):
+        lst[i], lst[~i] = lst[~i], lst[i]
+
+    return lst
+
+
+print(reverce_list([1, 2, 3, 4, 5, 6, 7, 8]))
+print(reverce_list([1, 2, 3, 4, 5, 6, 7]))
