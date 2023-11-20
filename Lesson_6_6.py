bc9fd136a690a7195578aaaea05eaa5aa843a902
rudinskivyacheslav <sslavabox@gmail.com>
2023-11-20 14:34:47 +0300

    new lesson_6_6

--- Lesson_6_6.py
+++ Lesson_6_6.py
@@ -2,5 +2,16 @@
 # чтобы остались только строки, использование дополнительного списка незаконно
 lst = [1, None, "True", {4, "Big"}, False, "Hello", (4, "Python"), [True, "Java"], 43, "Data", {1: "one", 2: "two"}]
 
-lst = list(filter(lambda x: isinstance(x, str), lst))
+# lst = list(filter(lambda x: isinstance(x, str), lst))
+
+def reverce(lst: list):
+    index = 0
+    while index < len(lst):
+        if not isinstance(lst[index], str):
+            lst.pop(index)
+        else:
+            index += 1
+
+
+reverce(lst)
 print(lst)
