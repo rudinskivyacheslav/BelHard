61a3fc6f4ed444cfe6950388d738a4789412859f
rudinskivyacheslav <sslavabox@gmail.com>
2023-11-20 13:41:28 +0300

    new lesson_6_3

--- Lesson_6_3.py
+++ Lesson_6_3.py
@@ -1,18 +1,18 @@
 # Дан словарь, ключ - Название страны, значение - список городов, на вход поступает город,
 # необходимо сказать из какой он страны
-globus = {"Belarus": ["Minsk", "Gomel", "Grodno"], "Usa": ["New York", "Houston", "Chicago", "Los Angeles"],
+globus = {"Belarus": ["Minsk", "Gomel", "Grodno"],
+          "Usa": ["New York", "Houston", "Chicago", "Los Angeles"],
           "Poland": ["Varshava", "Krakov", "Wroclav"]}
 
 
 def get_country(city_search: str):
+    city_search = f"{city_search[0].upper()}{city_search[1:].lower()}"
     for counry, citys in globus.items():
-        for city in citys:
-            if city_search.lower() == city.lower():
-                return counry
-    else:
-        return "No data"
+        if city_search in citys:
+            return counry
+    return "No data"
 
 
-print(get_country("miNSK"))
+print(get_country("GoMel"))
 print(get_country("Osipovichi"))
 print(get_country("Houston"))
